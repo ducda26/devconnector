9d82978863722f2d93741800f9dee47873310670
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-14 15:13:31 +0700

    3. Connecting To MongoDB With Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 /*Set up ban dau
 + npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
 + npm i -D nodemon concurrently
+
+npm run server
 */
 
 const express = require('express');
+const connectDB =require('./config/db')
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 

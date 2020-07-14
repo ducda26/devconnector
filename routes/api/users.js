4cc53b2ad863532c352119cd6d78c7694bab188d
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-14 20:42:35 +0700

    install json web token va set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator');
 //@route POST api/users
 //@desc register route
@@ -36,7 +38,7 @@ router.post(
             let user = await User.findOne({ email });
 
             if (user) {
-               return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
             }
             // Get users gravatar
             const avatar = gravatar.url(email, {
@@ -55,11 +57,28 @@ router.post(
             // Rncrypt password
             const salt = await bcrypt.genSalt(10);
 
-            user.password = await bcrypt.hash(password,salt)
+            user.password = await bcrypt.hash(password, salt)
 
             await user.save();
             // Return jsonwebtoken
-            res.send('User registered')
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 36000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+
+            //res.send('User registered')
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error')

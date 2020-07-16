c96264a26c6467c422a0cdca3c0b3fb96ca1af41
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-16 12:28:18 +0700

    Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,8 @@ const auth = require('../../middleware/auth');
 const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+
+
 //@route POST api/posts
 //@desc create post
 //@accsess Private
@@ -45,5 +47,77 @@ router.post('/',
         }
     }
 )
+
+//@route GET api/posts
+//@desc Get all post
+//@accsess Private
+
+router.get('/', auth, async (req,res)=>{
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+
+
+//@route GET api/posts/:id
+//@desc Get post by ID
+//@accsess Private
+
+router.get('/:id', auth, async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+
+        res.json(post)
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind==='ObjectId') {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+
+//@route DELETE api/posts/:id
+//@desc Dlete a post
+//@accsess Private
+
+router.delete('/:id', auth, async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({msg: 'Post not found'});
+        }
+
+        // Chack User
+        if(post.user.toString()!==req.user.id){
+            return res.status(401).json({msg:'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch (err) {
+        console.error(err.message);
+
+        if(err.kind==='ObjectId') {
+            return res.status(404).json({msg: 'Post not found'})
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+
+
 
 module.exports = router

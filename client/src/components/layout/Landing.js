528f9bc6e8830424629db945296cfdf9d52e01d2
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-20 12:37:32 +0700

    Starting On The Dashboard

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types'
 
 
-const Landing = () => {
+
+const Landing = ({isAuthenticated}) => {
+  if (isAuthenticated){
+    return <Redirect to='/dashboard' />
+  }
+  
+
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -20,6 +28,13 @@ const Landing = () => {
       </div>
     </section>
     )
+};
+
+Landing.propTypes ={
+  isAuthenticated: PropTypes.bool
 }
 
-export default Landing
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+export default connect(mapStateToProps) (Landing)
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -8,8 +8,8 @@ const initialState = {
     error: {}
 }
 
-export default function (state = initialState, action){
-    const {type, payload} = action;
+export default function (state = initialState, action) {
+    const { type, payload } = action;
 
     switch (type) {
         case GET_PROFILE:
@@ -24,7 +24,13 @@ export default function (state = initialState, action){
                 error: payload,
                 loading: false
             };
-    
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
+                loading: false
+            }
         default:
             return state;
     }

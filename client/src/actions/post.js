7e1c749f9a667f473e6861f952d28452d742ab96
ducda26 <63221856+ducda26@users.noreply.github.com>
2020-07-21 17:41:25 +0700

    6. Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  GET_POST
 } from './types';
 
 // Get posts
@@ -14,7 +15,7 @@ export const getPosts = () => async dispatch => {
     const res = await axios.get('/api/posts');
 
     dispatch({
-      type: GET_POSTS,
+      type: GET_POST,
       payload: res.data
     });
   } catch (err) {
@@ -99,6 +100,23 @@ export const addPost = formData => async dispatch => {
     });
 
     dispatch(setAlert('Post Created', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get posts
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,19 +1,18 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getPosts } from '../../actions/post';
 import PostItem from './PostItem';
-import Spinner from '../layout/Spinner';
 import PostForm from './PostForm';
+import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: { posts, loading} }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? (<Spinner />) :
-    (<Fragment>
-       <h1 className="large text-primary">Posts</h1>
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
       <p className="lead">
         <i className="fas fa-user" /> Welcome to the community
       </p>
@@ -24,7 +23,7 @@ const Posts = ({ getPosts, post: { posts, loading} }) => {
         ))}
       </div>
     </Fragment>
-    )
+  );
 };
 
 Posts.propTypes = {
